91234c666af233d0dbbe5a3591430a03b82a077b
olaberglund <olakjberglund@gmail.com>
2021-11-12 14:54:40 +0000

    added anti-anti-scraping headers

--- olaberglund/scraper/js/bloombergScraper.js
+++ olaberglund/scraper/js/bloombergScraper.js
@@ -5,7 +5,12 @@ const URL = "https://www.bloomberg.com/markets/stocks/world-indexes/asia-pacific
 async function getRawData (url) {
   const {data} = await axios.get(URL, {
   headers: {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+    'cache-control': 'max-age=0',
+    'upgrade-insecure-requests': '1'
   }
   });
   return data;
@@ -21,19 +26,15 @@ async function getChange (name) {
 }
 
 async function getIndicies() {
-  try {
-    const nikkei = getChange('NIKKEI 225');
-    const shanghai = getChange('SHANGHAI SE COMPOSITE');
-    const hangseng = getChange('HANG SENG INDEX');
-    const changes = await Promise.all([nikkei, shanghai, hangseng]);
-    const message = "Nikkei: " + changes[0] + "\nShanghai composite: " + changes[1] + "\nHang Seng: " + changes[2];
-    return message;
-  }catch (e) {
-    return "Error: bloombergScraper";
-  }
+  const nikkei = getChange('NIKKEI 225');
+  const shanghai = getChange('SHANGHAI SE COMPOSITE');
+  const hangseng = getChange('HANG SENG INDEX');
+  const changes = await Promise.all([nikkei, shanghai, hangseng]);
+  return changes;
 }
 
 getIndicies().then( (changes) => {
   console.log(changes)
 });
+
 
