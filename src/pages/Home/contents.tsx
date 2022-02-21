fda7ef89162f0864cb71cda213d3fcd5ece40cd9
olaberglund <olakjberglund@gmail.com>
2022-02-21 22:47:05 +0100

    create contents for home page

---
+++ src/pages/Home/contents.tsx
@@ -0,0 +1,15 @@
+export const homeContent = {
+  title: "Hem",
+  message: `Hejsan! Min förra sida såg inte alls bra ut. Här har jag slängt upp
+  en enkel sida m.h.a. React med en idé jag fick när jag såg en lp-spelare.`
+}
+
+export const aktuelltContent = {
+  title: "Aktuellt",
+  message: `Jag har nyligen fått jobb på Lunicore och ser fram emot det!`
+}
+
+export const legacy = {
+  title: "Legacy",
+  message: `Hade ett jobb på Redeye som skribent. Ta en kik på alla morgonbrev här på min sida!`
+}
