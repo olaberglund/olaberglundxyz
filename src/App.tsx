e452cc880e9038b8f97b49a43511d8dfe5f66cfd
olaberglund <olakjberglund@gmail.com>
2022-02-25 18:43:07 +0100

    first commikt

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import Home from './pages/Home/Home';
-import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}>
-          <Route />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
