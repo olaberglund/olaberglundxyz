97a32f346da92e2057929996ee7c8240825b6474
olaberglund <olakjberglund@gmail.com>
2022-02-25 19:19:53 +0100

    make them accessible

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import GlobalStyle from './styles/GlobalStyles';
+import {} from 'styled-components/cssprop'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  function App() {
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <div className="App">
+        </div>
+      </ThemeProvider>
+    );
+  }
 
 export default App;
