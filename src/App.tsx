13a133b681ffcb2bca2ddb76f2e7930d28385e03
olaberglund <ola.kj.berglund@gmail.com>
2022-02-26 13:26:07 +0100

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
+import  theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyles';
-import {} from 'styled-components/cssprop'
 
   function App() {
     return (
