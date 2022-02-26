8115f4c1f237a48535fe967d8679820b8f6f2394
olaberglund <ola.kj.berglund@gmail.com>
2022-02-26 18:34:22 +0100

    define secondary theme color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,13 +2,14 @@ export type ThemeType = typeof light;
 
 export const light = {
   primary: "#FFF",
+  secondary: "#FFF",
   text: "#000",
   background: "#111",
 }
 
 export const dark: ThemeType = {
-  primary: '#8CA3A6',
-  //text: '#385154',
+  primary: '#385154',
+  secondary: '#8CA3A6',
   text: '#FFF',
   background: '#1C1C1C',
 }
