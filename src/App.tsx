d05395bf0c0f7e5a90c55d700a6221a2a4945cfd
olaberglund <ola.kj.berglund@gmail.com>
2022-02-26 18:33:53 +0100

    create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import  theme from './styles/theme';
+import theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
 
-  function App() {
-    return (
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <div className="App">
-        </div>
-      </ThemeProvider>
-    );
-  }
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<div>Not Found!</div>} />
+          </Route>
+        </Routes>
+      </Router>
+
+    </ThemeProvider>
+  );
+}
 
 export default App;
