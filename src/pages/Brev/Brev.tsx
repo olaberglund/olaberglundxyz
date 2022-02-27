82f790e3770355a56caf405bdf9a2fd8316e0087
olaberglund <olakjberglund@gmail.com>
2022-02-27 21:31:00 +0100

    create basic brev page

---
+++ src/pages/Brev/Brev.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout, Sidebar } from './Styled.Brev'
+
+function Brev() {
+  return (
+    <Layout>
+      <Sidebar>
+      </Sidebar>
+      <button>yo</button>
+    </Layout>
+  )
+}
+
+export default Brev
