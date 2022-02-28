7416f20973f09d77da086c58ac9de0ddd75feecf
olaberglund <olakjberglund@gmail.com>
2022-02-28 22:30:11 +0100

    test downloading a file

--- src/components/BrevDataTable.tsx
+++ src/components/BrevDataTable.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { storage } from '../lib/firebase';
+import { ref, getDownloadURL } from 'firebase/storage'
 
 function BrevDataTable() {
+
+  useEffect(() => {
+    const lettersRef = ref(storage, 'Brev/Blodröd öppning väntas(1).docx');
+    getDownloadURL(lettersRef)
+      .then((url) => {
+        const xhr = new XMLHttpRequest();
+        console.log(url);
+        xhr.responseType = 'blob';
+        xhr.onload = (event) => {
+          const blob = xhr.response;
+        };
+        xhr.open('GET', url);
+        xhr.send();
+      })
+  }, [])
+
   return (
-    <div>BrevDataTable</div>
+    <div>Ladda ned?</div>
   )
 }
 
