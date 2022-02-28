b158d072cf1220ac238c89f318d7f296cb268fc7
olaberglund <olakjberglund@gmail.com>
2022-02-28 21:05:26 +0100

    extract links to js object

--- src/pages/Brev/Brev.tsx
+++ src/pages/Brev/Brev.tsx
@@ -4,26 +4,37 @@ import FeedIcon from '@mui/icons-material/Feed';
 import CreateIcon from '@mui/icons-material/Create';
 import { QuestionMark } from '@mui/icons-material';
 import { Route, Routes } from 'react-router-dom';
+import BrevDataTable from '../../components/BrevDataTable';
 
 function Brev() {
+
+  const links = {
+    alla: "alla",
+    nytt: "nytt",
+    faq: "faq",
+  }
+
   return (
     <Layout>
       <Sidebar>
-        <SidebarElement to="alla">
+        <SidebarElement to={links.alla}>
           <FeedIcon />
           Se alla brev
         </SidebarElement>
-        <SidebarElement to="nytt">
+        <SidebarElement to={links.nytt}>
           <CreateIcon />
           Skriv nytt brev
         </SidebarElement>
-        <SidebarElement to="faq">
+        <SidebarElement to={links.faq}>
           <QuestionMark />
           Tips & tricks
         </SidebarElement>
       </Sidebar>
       <Routes>
-        <Route path="alla/*" element={<BrevDataTable />} />
+        <Route index element={<p>Inget att visa.</p>} />
+        <Route path={links.alla} element={<BrevDataTable />} />
+        <Route path={links.nytt} element={<p>Nytt brev</p>} />
+        <Route path={links.faq} element={<p>Faq</p>} />
       </Routes>
     </Layout>
   )
