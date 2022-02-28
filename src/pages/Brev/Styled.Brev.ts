159d9efe3a3bcb698fbfb3fec94c15639467a5ce
olaberglund <olakjberglund@gmail.com>
2022-02-28 20:14:43 +0100

    add keyframes animation for sliding

--- src/pages/Brev/Styled.Brev.ts
+++ src/pages/Brev/Styled.Brev.ts
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Layout = styled.div`
@@ -7,11 +8,30 @@ export const Layout = styled.div`
   flex: 1;
 `
 
-export const SidebarElement = styled.div`
+export const SidebarElement = styled(Link)`
+  @keyframes slideInFromLeft {
+    from {
+      transform: translateX(-100%);
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  gap: 8px;
+  justify-content: flex-start;
+  color: ${({ theme }) => theme.text};
+  text-decoration: none;
+  background-color: ${({ theme }) => theme.primary};
+  gap: 12px;
+  width: 100%;
+  padding: 10px;
+  animation: 0.3s ease-out slideInFromLeft;
+
+  :hover {
+    background-color: ${({ theme }) => theme.tertiary};
+  }
 `
 
 export const Sidebar = styled.div`
@@ -19,9 +39,14 @@ export const Sidebar = styled.div`
   justify-content: center;
   align-items: flex-start;
   flex-direction: column;
-  background-color: ${({ theme }) => theme.primary};
-  border-top-right-radius: 10px;
-  border-bottom-right-radius: 10px;
-  padding: 10px;
-  gap: 16px;
+
+
+  & > :last-child {
+    border-bottom-right-radius: 10px;
+  }
+
+  & > :first-child {
+    border-top-right-radius: 10px;
+  }
+
 `
