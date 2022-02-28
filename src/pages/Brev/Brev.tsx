8648c51fa0480c7a3adf4bfd60ac34d4669c6521
olaberglund <ola.kj.berglund@gmail.com>
2022-02-28 14:39:37 +0100

    create sidebar elements

--- src/pages/Brev/Brev.tsx
+++ src/pages/Brev/Brev.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Layout, Sidebar } from './Styled.Brev'
+import { SidebarElement, Layout, Sidebar } from './Styled.Brev'
+import FeedIcon from '@mui/icons-material/Feed';
+import CreateIcon from '@mui/icons-material/Create';
 
 function Brev() {
   return (
     <Layout>
       <Sidebar>
+        <SidebarElement>
+          <FeedIcon />
+          Se alla brev
+        </SidebarElement>
+        <SidebarElement>
+          <CreateIcon />
+          Skriv nytt brev
+        </SidebarElement>
       </Sidebar>
       <button>yo</button>
-    </Layout>
+    </Layout >
   )
 }
 
