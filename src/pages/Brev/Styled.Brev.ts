9da37d7418a94f805aaab6d91ef235e8db81811f
olaberglund <ola.kj.berglund@gmail.com>
2022-02-28 14:39:43 +0100

    create sidebar element

--- src/pages/Brev/Styled.Brev.ts
+++ src/pages/Brev/Styled.Brev.ts
@@ -4,13 +4,23 @@ export const Layout = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  height: 100%;
+`
+
+export const SidebarElement = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
 `
 
 export const Sidebar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
   background-color: ${({ theme }) => theme.primary};
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
-  width: 80px;
-  height: 100%;
+  padding: 10px;
+  gap: 16px;
 `
