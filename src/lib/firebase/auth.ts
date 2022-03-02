89ec212ce8d012fe20a58d67f0c50197c820ca75
olaberglund <olakjberglund@gmail.com>
2022-03-02 19:20:54 +0100

    remove unused constants

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,25 +1,16 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup } from "firebase/auth";
 import { provider } from "./firebase";
 
 export const signInWithGoogle = async () => {
   const auth = getAuth();
   signInWithPopup(auth, provider)
     .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential?.accessToken;
       // The signed-in user info.
       const user = result.user;
-      // ...
     }).catch((error) => {
-      // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+      console.error(errorCode, errorMessage);
     });
 }
 
