810c85d24143c3b3924a5839ee8ef3be6c129cb9
olaberglund <olakjberglund@gmail.com>
2022-03-02 22:29:53 +0100

    remove unnecessary conversion to bool

--- src/components/Navbar/UserStatusButton.tsx
+++ src/components/Navbar/UserStatusButton.tsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react'
-import { signInWithGoogle, signOut } from '../../lib/firebase/auth'
+import { signInWithGoogle, signOutUser } from '../../lib/firebase/auth'
 import { UserContext } from '../../lib/firebase/context'
 import { StyledLoginButton, StyledLogoutButton } from './LoginButton.styled'
 
 function UserStatusButton() {
-  const { name } = useContext(UserContext);
+  const { username } = useContext(UserContext);
 
   return (
-    Boolean(name) ?
-      <StyledLogoutButton onClick={signOut}>Logga ut {name}</StyledLogoutButton>
+    username ?
+      <StyledLogoutButton onClick={signOutUser}>Logga ut {username}</StyledLogoutButton>
       :
       <StyledLoginButton onClick={signInWithGoogle}>Logga in</StyledLoginButton>
   )
