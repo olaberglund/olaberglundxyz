63c07722a3633525681e2c0f1639e64ae3ed48e7
olaberglund <olakjberglund@gmail.com>
2022-03-02 22:30:44 +0100

    create a new user if one doersnt exist on login

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,19 +1,34 @@
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signOut, signInWithPopup } from "firebase/auth";
 import { provider } from "./firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "./firebase";
+import { User } from "./schemes";
+
+
+const auth = getAuth();
 
 export const signInWithGoogle = async () => {
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // The signed-in user info.
-      const user = result.user;
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(errorCode, errorMessage);
-    });
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    const userRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(userRef);
+
+    if (!docSnap.exists()) {
+      console.log("i was run");
+      await setDoc(userRef, {
+        name: user.displayName ?? user.email
+      } as User);
+    } else {
+      return;
+    }
+  } catch (error: any) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorCode, errorMessage);
+  }
 }
 
-export const signOut = () => {
-
+export const signOutUser = async () => {
+  await signOut(auth)
 }
