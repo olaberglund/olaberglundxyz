4c1de61b2a387c5fc26495e6cab2b85ba0ca835d
olaberglund <ola.kj.berglund@gmail.com>
2022-03-02 13:03:26 +0100

    create login button

---
+++ src/components/Navbar/LoginButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledLoginButton } from './LoginButton.styled'
+
+function LoginButton() {
+  return (
+    <StyledLoginButton>Logga in</StyledLoginButton>
+  )
+}
+
+export default LoginButton
