33ad210cceb51a4767acfacefae24ac93c3980c0
olaberglund <olakjberglund@gmail.com>
2022-03-02 19:21:57 +0100

    add firestore db to project

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
