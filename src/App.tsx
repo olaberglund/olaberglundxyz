267da6bf65019ad2bf28e3c754965a134c82ce10
olaberglund <olakjberglund@gmail.com>
2022-03-02 19:21:40 +0100

    add listening for user

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyles';
@@ -8,20 +8,37 @@ import Home from './pages/Home';
 import Brev from './pages/Brev/Brev';
 import { FlexLayout } from './styles/App.styled';
 import Traning from './pages/Traning/Traning';
+import { UserContext } from './lib/firebase/context';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './lib/firebase/firebase';
 
 function App() {
+  const [user] = useAuthState(auth)
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    //realtime subscription
+    let unsubscribe; 
+
+    if(user) {
+    }
+  }, [user])
+
   return (
+
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
         <FlexLayout>
-          <Navbar />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="brev/*" element={<Brev />} />
-            <Route path="traning/*" element={<Traning />} />
-            <Route path="*" element={<div>Not Found!</div>} />
-          </Routes>
+          <UserContext.Provider value={{name: "Jordan"}}>
+            <Navbar />
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="brev/*" element={<Brev />} />
+              <Route path="traning/*" element={<Traning />} />
+              <Route path="*" element={<div>Not Found!</div>} />
+            </Routes>
+          </UserContext.Provider>
         </FlexLayout>
       </Router>
     </ThemeProvider>
