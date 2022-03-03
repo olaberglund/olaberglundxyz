89b9df582e85c8587e434e6ff99dbf1fa2e0f4b8
olaberglund <olakjberglund@gmail.com>
2022-03-03 23:20:21 +0100

    make username persistent through refreshes

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -2,27 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { db, auth } from './firebase/firebase';
 import { doc, onSnapshot } from 'firebase/firestore';
+import { User } from 'firebase/auth';
 
 
-export const useUserData = () => {
-  const [user] = useAuthState(auth)
-  const [username, setUsername] = useState(null);
+export const useUserData = (): [string | null, User | null | undefined, boolean] => {
+  const [user, loading, error] = useAuthState(auth)
+  const [username, setUsername] = useState<string | null>(null);
 
   useEffect(() => {
     //realtime subscription
-    let unsubscribe; 
+    let unsubscribe;
+    //user variable changed and will initially be null during load
+    //so use previous username if there is one
+    setUsername(localStorage.getItem('name'));
 
-    if(user) {
+    // new user has successfully logged in
+    if (user) {
       const ref = doc(db, 'users', user.uid);
       unsubscribe = onSnapshot(ref, (doc) => {
-        setUsername(doc.data()?.name);
+        const uname = doc.data()?.name;
+        localStorage.setItem('name', uname);
+        setUsername(uname);
       })
-    }else {
-      setUsername(null);
+    } else {
+      // no user and auth isn't loading, therefore definitely logged out
+      if (!loading) {
+        setUsername(null)
+        localStorage.removeItem('name');
+      }
     }
 
-    return unsubscribe;
-  }, [user])
 
-  return username;
+    return unsubscribe;
+  }, [user]);
+
+  return [username, user, loading];
 }
