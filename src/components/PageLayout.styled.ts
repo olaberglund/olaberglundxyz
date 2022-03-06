0dadaa619880fd5ebad722dd4c0d72586812fe9e
olaberglund <ola.kj.berglund@gmail.com>
2022-03-06 21:19:03 +0100

    make column width of sidebar as wide as sidebar

--- src/components/PageLayout.styled.ts
+++ src/components/PageLayout.styled.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const PageLayout = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: auto 1fr 1fr;
   flex: 1;
-  gap: 10%;
+  gap: 5%;
 `
