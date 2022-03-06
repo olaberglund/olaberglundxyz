6768becc8adce9a04e7fabb5721bc7624fc44182
olaberglund <ola.kj.berglund@gmail.com>
2022-03-06 23:25:59 +0100

    specify only grid space for sidebar

--- src/components/PageLayout.styled.ts
+++ src/components/PageLayout.styled.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const PageLayout = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: auto 1fr 1fr;
+  grid-template-columns: auto 1fr;
   flex: 1;
-  gap: 5%;
 `
