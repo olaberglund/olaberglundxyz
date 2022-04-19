9d7facac5e77b108005fc46345653d8a8204c800
olaberglund <olakjberglund@gmail.com>
2022-04-19 13:48:00 +0200

    use index convention

---
+++ src/components/Programming/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Paper } from './styled';
+
+const Programming: React.FC = () => {
+
+    return (
+      <Paper>yo</Paper>
+    )
+
+}
+
+export default Programming;
---
+++ src/pages/Program/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Programming from '../../components/Programming'
+
+const Program = () => {
+
+  return (
+    <Layout>
+      <Programming />
+    </Layout>
+  )
+}
+
+export default Program;
+
+const Layout = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
